7a4b7d392f98adc308b39899d805a8eea6b7b1ef
enkuria <kengeene@gmail.com>
2024-06-10 16:06:42 +0300

    style: add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/css/App.css'
 import { useQuery, gql } from "@apollo/client";
 import {
   Button,
@@ -15,6 +13,10 @@ import {
   CardActions,
   Grid,
 } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+
 import {Book} from '@/types/index'
 function App() {
   const GET_BOOKS = gql`
@@ -32,45 +34,94 @@ function App() {
     if (loading) return <CircularProgress />;
     if (error) return <p>Error : {error.message}</p>;
 
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+      color: "inherit",
+      "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+          width: "20ch",
+        },
+      },
+    }));
+
+    const Search = styled("div")(({ theme }) => ({
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto",
+      },
+    }));
+
+    const SearchIconWrapper = styled("div")(({ theme }) => ({
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }));
+
   return (
     <>
-    <div>
-
-    </div>
+      <div className='my-4'>
+        {" "}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+      </div>
       <Grid container spacing={4}>
         {data.books.length > 0 ? (
-          data.books.map((book: Book) => (
-            <Grid item xs={4}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  src={`./${book.coverPhotoURL}`}
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {book.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {book.author}
-                  </Typography>
-                  <Stack direction="row" spacing={1} useFlexGap>
-                    Level:
-                    <Chip
-                      size="small"
-                      label={book.readingLevel}
-                      color="success"
-                    />
-                  </Stack>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-              </CardActions>
+          data.books.map((book: Book, index: number) => (
+            <Grid item xs={12} sm={4} md={4} lg={4} key={index}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    src={`./${book.coverPhotoURL}`}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {book.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {book.author}
+                    </Typography>
+                    <Stack direction="row" spacing={1} useFlexGap>
+                      Level:
+                      <Chip
+                        size="small"
+                        label={book.readingLevel}
+                        color="success"
+                      />
+                    </Stack>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Add to reading list
+                  </Button>
+                </CardActions>
               </Card>
             </Grid>
           ))
