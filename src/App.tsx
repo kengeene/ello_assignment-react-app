33c730aca986c578078f8cda2aec7346f58bb4b4
enkuria <kengeene@gmail.com>
2024-06-10 17:00:21 +0300

    refactor: move search to dedicated compoennt

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import { useQuery, gql } from "@apollo/client";
 import {
   CircularProgress,
 } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
 import BooksList from '@/components/BooksList'
+import SearchBar from '@/components/SearchBar';
+
 
 function App() {
   const GET_BOOKS = gql`
@@ -24,59 +23,12 @@ function App() {
     if (loading) return <CircularProgress />;
     if (error) return <p>Error : {error.message}</p>;
 
-    const StyledInputBase = styled(InputBase)(({ theme }) => ({
-      color: "inherit",
-      "& .MuiInputBase-input": {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("md")]: {
-          width: "20ch",
-        },
-      },
-    }));
 
-    const Search = styled("div")(({ theme }) => ({
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: alpha(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto",
-      },
-    }));
-
-    const SearchIconWrapper = styled("div")(({ theme }) => ({
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    }));
 
   return (
     <>
       <div className="my-4">
-        {" "}
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search…"
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Search>
+       <SearchBar/>
       </div>
      <BooksList books={data.books}/>
     </>
