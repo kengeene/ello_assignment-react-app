5f7d90ea8cc5a263add7cee0fd11dd09ba252508
enkuria <kengeene@gmail.com>
2024-06-11 13:36:14 +0300

    style: edit color of searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import SearchIcon from "@mui/icons-material/Search";
 
-import { styled, alpha } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import { useEffect, useRef } from "react";
 
@@ -10,7 +10,6 @@ export default function SearchBar({ search, setSearch }: { search: string; setSe
     inputRef.current?.focus();
   }, [search])
     const StyledInputBase = styled(InputBase)(({ theme }) => ({
-      color: "inherit",
       "& .MuiInputBase-input": {
         // padding: theme.spacing(1, 1, 1, 0),
         // // vertical padding + font size from searchIcon
@@ -25,11 +24,12 @@ export default function SearchBar({ search, setSearch }: { search: string; setSe
 
   const Search = styled("div")(({ theme }) => ({
     position: "relative",
+    border: "1px solid var(--color-1)",
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
+    // backgroundColor: alpha(theme.palette.common.white, 0.15),
+    // "&:hover": {
+    //   backgroundColor: alpha(theme.palette.common.white, 0.25),
+    // },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
@@ -41,6 +41,7 @@ export default function SearchBar({ search, setSearch }: { search: string; setSe
 
   const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
+    color: "var(--color-3)",
     height: "100%",
     position: "absolute",
     pointerEvents: "none",
