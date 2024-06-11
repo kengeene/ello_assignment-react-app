a049da4251d864f57c016e8a5172f428afb0bd6b
enkuria <kengeene@gmail.com>
2024-06-11 14:51:32 +0300

    style: add reading list badge

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/pages/index.tsx'
 import '@/assets/css/index.css'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import {
   ApolloClient,
@@ -14,10 +15,39 @@ import {
     cache: new InMemoryCache(),
   });
 
+// declare module "@mui/material/styles" {
+//   interface Theme {
+//     palette: {
+//       light: string;
+//       main: string;
+//       dark: string;
+//       contrastText: string;
+//     };
+//   }
+// }
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#CFFAFA",
+        main: "#5ACCCC",
+        dark: "#28B8B8",
+        contrastText: "#FFFFFF",
+      },
+      warning: {
+        light: "#FABD33",
+        main: "#FABD33",
+        dark: "#FAAD00",
+        contrastText: "#FFFFFF",
+      },
+    },
+  });
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <ApolloProvider client={client}>
         <App />
     </ApolloProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import '@/assets/css/App.css'
-import { CircularProgress, Grid } from "@mui/material";
+import { CircularProgress, Badge, Grid } from "@mui/material";
 import BooksList from '@/components/BooksList'
 import SearchBar from '@/components/SearchBar';
 import useBooks from '@/hooks/useBooks';
 // import {AppBar} from '@mui/material';
-import ReadingListTable from '@/components/ReadingListTable'
+import ReadingListTable from '@/components/ReadingListTable';
+// import MailIcon from "@mui/icons-material/Mail";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 
 function App() {
   const { loading, error, data, setSearch, search, readingList } =
@@ -15,19 +17,25 @@ function App() {
 
   return (
     <Grid container spacing={2} minWidth={"90vw"}>
+      <Grid item xs={12} sm={12} md={12} lg={12} className="text-right">
+        <Badge
+          badgeContent={4}
+          color="warning"
+          className="hover:cursor-pointer"
+        >
+          <LocalLibraryIcon color="primary" />
+        </Badge>
+      </Grid>
       <Grid item xs={12} sm={12} md={12} lg={12} key={"top-bar"}>
         <div className="my-4">
-          <SearchBar setSearch={setSearch} search={search} />
+          <SearchBar setSearch={setSearch} search={search} />{" "}
         </div>
       </Grid>
       <Grid>
         <ReadingListTable readingList={readingList} />
       </Grid>
       <Grid item xs={12} sm={12} md={12} lg={12} key={"app-container"}>
-        <BooksList
-          books={data.books}
-          search={search}
-        />
+        <BooksList books={data.books} search={search} />
       </Grid>
     </Grid>
   );
